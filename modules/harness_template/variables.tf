07e8abecbf9f4043b1e488015dad3c55344c7e00
vishal-avyka <greeshma.g.gokuldas@avyka.com>
2026-06-29 15:43:35 +0530

    fix: expand single-line variable blocks to multi-line format for OpenTofu compatibility

--- modules/harness_template/variables.tf
+++ modules/harness_template/variables.tf
@@ -1,12 +1,53 @@
-variable "account_id"        { type = string }
-variable "org_id"            { type = string  default = "" }
-variable "project_id"        { type = string  default = "" }
-variable "is_project_scope"  { type = bool }
-variable "is_org_scope"      { type = bool }
-variable "is_account_scope"  { type = bool }
-variable "template_spec"     { type = any }
-variable "git_source_url"    { type = string }
-variable "git_connector_ref" { type = string  default = "" }
-variable "github_repo"       { type = string  default = "" }
-variable "github_branch"     { type = string  default = "main" }
-variable "template_yaml_path"{ type = string  default = "template.yaml" }
+variable "account_id" {
+  type = string
+}
+
+variable "org_id" {
+  type    = string
+  default = ""
+}
+
+variable "project_id" {
+  type    = string
+  default = ""
+}
+
+variable "is_project_scope" {
+  type = bool
+}
+
+variable "is_org_scope" {
+  type = bool
+}
+
+variable "is_account_scope" {
+  type = bool
+}
+
+variable "template_spec" {
+  type = any
+}
+
+variable "git_source_url" {
+  type = string
+}
+
+variable "git_connector_ref" {
+  type    = string
+  default = ""
+}
+
+variable "github_repo" {
+  type    = string
+  default = ""
+}
+
+variable "github_branch" {
+  type    = string
+  default = "main"
+}
+
+variable "template_yaml_path" {
+  type    = string
+  default = "template.yaml"
+}
