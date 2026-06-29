0fdfaf76a6e6dbed97e50439a70ce9d4676fc436
greeshmaggokuldas <greeshma.g.gokuldas@avyka.com>
2026-06-29 14:30:19 +0530

    Add files via upload

---
+++ modules/harness_opa/outputs.tf
@@ -0,0 +1,9 @@
+locals {
+  _policy = (
+    var.is_project_scope ? try(harness_platform_policy.project[0], null) :
+    var.is_org_scope     ? try(harness_platform_policy.org[0],     null) :
+                           try(harness_platform_policy.account[0], null)
+  )
+}
+output "policy_identifier" { value = try(local._policy.identifier, null) }
+output "git_source_url"    { value = var.git_source_url }
---
+++ modules/harness_opa/variables.tf
@@ -0,0 +1,8 @@
+variable "account_id"       { type = string }
+variable "org_id"           { type = string  default = "" }
+variable "project_id"       { type = string  default = "" }
+variable "is_project_scope" { type = bool }
+variable "is_org_scope"     { type = bool }
+variable "is_account_scope" { type = bool }
+variable "opa_spec"         { type = any }
+variable "git_source_url"   { type = string }
